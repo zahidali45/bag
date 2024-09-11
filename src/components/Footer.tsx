777c377d178ef4583cae877bbfcc1f174c93bb4e
tsc <mz260959@gmail.com>
2024-09-11 17:42:23 +0500

    new project

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div><div className='bg-slate-600 text-center '>
+    <h3>This is footer</h3>
+  </div>
+      
+    </div>
+  )
+}
+
+export default Footer
